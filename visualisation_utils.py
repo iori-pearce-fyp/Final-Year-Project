0aa212296a5d3c3a18f7a5eaa7eaf85625813e17
iori-pearce-fyp <i.pearce-22@student.lboro.ac.uk>
2024-11-20 14:58:29 +0000

    Added incoming start arrow to diagram function
    Created an invisible 'start' node to allow for an incoming arrow to the initial state node

--- visualisation_utils.py
+++ visualisation_utils.py
@@ -7,14 +7,20 @@ def produce_la_visual_representation(la):
     f = graphviz.Digraph('limited_automata', filename='la.gv')
     f.attr(rankdir='LR', size='8,5')
 
+    # Add an invisible start node for the incoming arrow
+    f.node('', shape='none')
+
+    # Iterate over the all states that are not accepting states and create a normal node
     f.attr('node', shape='circle')
     for node in set(la.states).difference(set(la.accepting_states)):
         f.node(f"{node}")
 
+    # Iterate over the accepting states and create a double circle node for each
     f.attr('node', shape='doublecircle')
     for node in la.accepting_states:
         f.node(f"{node}")
     
+    # Iterate over each transition in the transition function and create edges between nodes that correspond to the transitions
     for (transition_key, transition_value) in la.transition_function.items():
 
         print(transition_key, transition_value)
@@ -26,5 +32,8 @@ def produce_la_visual_representation(la):
         value_state = transition_value[2]
 
         f.edge(key_state, value_state, label=f"{key_char}/{value_rewrite},{value_movement}")
+
+    # Create the incoming start arrow to the initial state node
+    f.edge('', la.initial_state)
 
     f.view()
